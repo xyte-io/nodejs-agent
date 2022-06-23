8696be740934b9b21de6bbd080b1db1f6c03f0bc
Vladimir Okun <24977978+okvv@users.noreply.github.com>
2022-06-23 14:32:29 +0300

    * update main. type fields in package json to point to main.js files * remove unused template files

--- src/helpers/uuid.ts
+++
@@ -1,5 +0,0 @@
-import crypto from "node:crypto";
-
-const UUID = (options?: crypto.RandomUUIDOptions | undefined): string => crypto.randomUUID(options);
-
-export default UUID;
--- src/index.ts
+++
@@ -1,3 +0,0 @@
-import uuid from "./helpers/uuid";
-
-export default uuid;
