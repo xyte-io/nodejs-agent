00b16a5aef28fb75c8258789fc653d3e7bc3d6ce
Vladimir Okun <24977978+okvv@users.noreply.github.com>
2022-06-23 15:09:00 +0300

    switch to .cjs in eslint as we're using "type": "module" in this repo and it won't work otherwise

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,7 +1,7 @@
 module.exports = {
   parser: '@typescript-eslint/parser',
   parserOptions: {
-    sourceType: "module",
+    sourceType: 'module',
     tsconfigRootDir: __dirname,
     project: ['./tsconfig.json'],
   },
