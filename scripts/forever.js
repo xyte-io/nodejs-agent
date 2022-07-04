ae2f57d4eb1bab43cbebfc12e6a5cc4c220ae4fc
Vladimir Okun <24977978+okvv@users.noreply.github.com>
2022-07-04 19:03:04 +0300

    add log files produced by forever monitor.

--- scripts/forever.js
+++ scripts/forever.js
@@ -5,18 +5,22 @@ const child = new forever.Monitor('./node-agent-main.min.js', {
   killTree: true,
   watch: false,
   args: [],
+  // Log files and associated logging options for this instance
+  logFile: './logFile.log', // Path to log output from forever process (forever-monitor as a daemon)
+  outFile: './out.log', // Path to log output from child stdout
+  errFile: './err.log', // Path to log output from child stderr
 });
 
 child.on('restart', function () {
-  console.error('Forever restarting Xyte`s agent for ' + child.times + ' time');
+  console.error('#Forever restarting Xyte`s agent for ' + child.times + ' time');
 });
 
 child.on('exit:code', function (code) {
-  console.error('Forever detected Xyte`s agent exited with code ' + code);
+  console.error('#Forever detected Xyte`s agent exited with code ' + code);
 });
 
 child.on('exit', function () {
-  console.log('Xyte`s agent has exited');
+  console.log('#Xyte`s agent has exited');
 });
 
 child.start();
