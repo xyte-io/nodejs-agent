7abf758cb7bc4794b9728106fb21f9277a8ce726
Vladimir Okun <24977978+okvv@users.noreply.github.com>
2022-07-07 13:23:25 +0300

    remove unused PARTNER_KEY as we've migrated to using HARDWARE_KEY

--- src/helpers/constants.ts
+++ src/helpers/constants.ts
@@ -1,5 +1,4 @@
 export const HARDWARE_KEY = 'cb04432e-b465-4f08-a50d-9881b71aceab';
-export const PARTNER_KEY = '122331231231123';
 export const NANO_ID = '123412341234';
 export const FIRMWARE_VERSION = '1.0.0';
 export const CONFIG_FILE_NAME = 'config.txt';
